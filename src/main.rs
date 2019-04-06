5273f1e27d06979bf4caf121b1a2e72c6fda8c95
Stefan Mesken <kappadistributive@gmail.com>
2019-04-06 16:06:51 +0200

    fixed a minor problem with dotenv

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+use rocket_contrib::json::Json;
+use serde_json::Value;
+
+use dotenv::dotenv;
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+
+#[macro_use]
+extern crate diesel;
+
+mod db;
+use crate::db::*;
+mod models;
+use crate::models::*;
+mod onvista;
+mod schema;
+
+#[get("/<isin>")]
+fn newest(isin: String) -> Json<Value> {
+    let database_url = env::var("DATABASE_URL").expect("Set DATABASE_URL");
+    match Price::newest_by_isin(isin, &establish_connection(&database_url)) {
+        // todo: make Serialize work for price and replace all of this
+        Some(price) => Json(json!({
+            "status": 200,
+            "result": json!({
+                "Name": price.name,
+                "ISIN": price.isin,
+                "Kind": price.kind,
+                "Date": format!("{}",price.time.format("%Y-%m-%d")),
+                "Time": format!("{}",price.time.format("%H:%M:%S %z")),
+                "Price": format!("{}", price.price),
+                "Currency": price.currency}),
+        })),
+        None => Json(json!({
+            "status": 404,
+            "result": null,
+        })),
+    }
+}
+
+fn main() {
+    dotenv().ok();    
+    rocket::ignite()
+        .mount("/rstock/newest/", routes![newest])
+        .launch();
+}
