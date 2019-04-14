a4d178bbab6dc79179c11772d42842c2f48bba1b
Stefan Mesken <kappadistributive@gmail.com>
2019-04-14 15:48:43 +0200

    added capabilities to add to db from csv files

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,9 +1,15 @@
 #[macro_use]
 extern crate diesel;
+use bigdecimal::BigDecimal;
+use chrono::{TimeZone, Utc};
+use clap::{App, Arg, SubCommand};
+use csv::ReaderBuilder;
+use std::str::FromStr;
 
 mod db;
-use crate::db::*;
+use crate::db::{connect, insert_prices};
 mod models;
+use models::NewPrice;
 mod onvista;
 mod schema;
 
@@ -13,6 +19,61 @@ use std::env;
 fn main() {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("Set DATABASE_URL");
-    let conn = connect(database_url).get().unwrap ();
-    update_prices(&conn);
+    let conn = connect(database_url).get().unwrap();
+
+    let matches = App::new("Rstock Daemon")
+        .version("0.1")
+        .subcommand(
+            SubCommand::with_name("with_csv")
+                .about("Adds provided prices to the price database.")
+                .arg(
+                    Arg::with_name("path")
+                        .value_name("PATH")
+                        .help("Provide stock price data in a csv.")
+                        .takes_value(true)
+                        .required(true),
+                ),
+        )
+        .get_matches();
+
+    // Data needs to be provided in the form of a csv whose rows are
+    // of the following format:
+    // <NAME>,<ISIN>,<KIND>,<%H:%M%:%S d%.m%.Y%> in UTC,<PRICE>,<CURRENCY>
+    // There is no verfification process of the validity of provided data!
+    if let Some(sc) = matches.subcommand_matches("with_csv") {
+        let mut reader = ReaderBuilder::new()
+            .has_headers(false)
+            .from_path(sc.value_of("path").expect("Couldn't find csv."))
+            .expect("Couldn't read csv.");
+        let mut prices = Vec::<NewPrice>::new();
+
+        for entry in reader.records() {
+            match entry {
+                Ok(column) => {
+                    let mut data = Vec::<String>::new();
+                    for item in column.iter() {
+                        data.push(String::from(item));
+                    }
+                    println!("{:?}", data);
+                    prices.push(NewPrice {
+                        name: data[0].clone (),
+                        isin: data[1].clone (),
+                        kind: data[2].clone (),
+                        time: Utc
+                            .datetime_from_str(&data[3], "%H:%M:%S %d.%m.%Y")
+                            .expect("Couldn't parse date.")
+                            .with_timezone(&Utc),
+                        price: BigDecimal::from_str(&data[4])
+                            .expect("Couldn't parse price."),
+                        currency: data[5].clone (),
+                    });
+                }
+                Err(_) => println!("Missing entry!"),
+            }
+        }
+        insert_prices(&conn,prices);
+    } else {
+        println!("Run update_prices")
+        // update_prices(&conn);
+    }
 }
--- src/db.rs
+++ src/db.rs
@@ -1,27 +1,33 @@
-use crate::models::Item;
+use crate::models::{Item, NewPrice};
 use crate::onvista::*;
 use crate::schema::prices;
 use diesel::prelude::*;
 use diesel::r2d2::ConnectionManager;
 
 pub type Pool = diesel::r2d2::Pool<ConnectionManager<PgConnection>>;
-// pub fn establish_connection(database_url: &String) -> diesel::pg::PgConnection {    
-//     diesel::pg::PgConnection::establish(&database_url)
-//         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-// }
 
-// pub struct Connection(pub PooledConnection<ConnectionManager<PgConnection>>);
-
+#[allow (dead_code)]
 pub fn connect(database_url: String) -> Pool {
     let manager = ConnectionManager::<PgConnection>::new (database_url);
     Pool::new(manager).expect ("Failed to create pool.")
 }
 
+#[allow (dead_code)]
 pub fn update_prices(conn: &PgConnection) {
     let watchlist = Item::all(conn);
     for item in watchlist {
         diesel::insert_into(prices::table)
             .values(&onvista_etf_now(item.isin))
             .execute(conn).ok();
+    }
+}
+
+#[allow (dead_code)]
+pub fn insert_prices(conn: &PgConnection, prices: Vec<NewPrice>) {
+    for price in prices {
+        diesel::insert_into(prices::table)
+            .values(&price)
+            .execute(conn).ok();
+        println!("Inserted {:#?}", price);
     }
 }
